48f7cfaea363a70ff1f0b1495ed37fef8e1f52d5
gra-m <grahamd483@gmail.com>
2023-06-12 14:25:17 +0200

    39b. Module kafka-config-data/Resources application.yml continued. BeanConfiguration and OrderServiceApplication classes added

---
+++ order-service/order-container/src/main/java/com/food/ordering/system/order/service/domain/BeanConfiguration.java
@@ -0,0 +1,23 @@
+package com.food.ordering.system.order.service.domain;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Spring managed configuration bean
+ * In order to keep OrderDomainService [in fact the Domain Core of the entire order service]  independent from
+ * external dependencies it is instead tagged as SpringBean here.
+ */
+
+@Configuration
+public class BeanConfiguration {
+
+/**
+ * Registering OrderDomainService as a SpringBean
+ * @return SpringBean OrderDomainService
+ */
+@Bean
+public OrderDomainService orderDomainService() {
+    return new OrderDomainServiceImpl();
+}
+}
---
+++ order-service/order-container/src/main/java/com/food/ordering/system/order/service/domain/OrderServiceApplication.java
@@ -0,0 +1,33 @@
+package com.food.ordering.system.order.service.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * note:
+ * SpringBootApplication
+ * scanBasePackages: important when working with multiple modules any module in project will be scanned by Spring as
+ * long as it starts with "com.food.ordering.system".
+ * EntityScan
+ * basePackages: requirement for multi-module project the jpa entity classes are strictly held in the dataaccess package
+ * EnableJpaRepositories
+ * basePackages: dataaccess repos are also strictly held in dataacess package
+ *
+ */
+@EnableJpaRepositories(basePackages = "com.food.ordering.system.order.service.dataaccess")
+@EntityScan(basePackages = "com.food.ordering.system.order.service.dataaccess")
+@SpringBootApplication(scanBasePackages = "com.food.ordering.system")
+public class OrderServiceApplication {
+
+
+/**
+ * This class and any args are passed to SpringApplication.run
+ *
+ * @param args as required for the business logic of this Spring microservice
+ */
+static void main(String[] args) {
+    SpringApplication.run(OrderServiceApplication.class, args);
+}
+}
