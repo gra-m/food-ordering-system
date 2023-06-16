e53f7644440c6205e354bcccaf0eb4094362e454
gra-m <grahamd483@gmail.com>
2023-06-16 10:09:41 +0200

    43b. Payment Service [M]->domain-core Exceptions. Abstract Payment Event implements DomainEvent and makes subclasses templates

---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/com/food/ordering/system/payment/service/domain/event/PaymentEvent.java
@@ -0,0 +1,44 @@
+package com.food.ordering.system.payment.service.domain.event;
+
+import com.food.ordering.system.domain.event.DomainEvent;
+import com.food.ordering.system.payment.service.domain.entity.Payment;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+/**
+ * Generic DomainEvent is being used as a marker class for all domain events, here PaymentEvent accommodates
+ * Payment objects -> PaymentCompleted/Cancelled/FailedEvent but this can easily work for
+ * Booking objects -> BookingCompleted/Cancelled/FailedEvent
+ */
+public abstract class PaymentEvent implements DomainEvent<Payment> {
+
+/**
+ * By making these uninitialised variables final, a constructor is required in order to initialise them.
+ * This enforces the instantiation of these variables within any extending class. So here an abstract class is
+ * being used to enforce a template on all inheriting classes.
+ */
+private final Payment payment;
+private final ZonedDateTime createdAt;
+private final List<String> failureMessages;
+
+
+protected PaymentEvent(Payment payment, ZonedDateTime createdAt, List<String> failureMessages) {
+    this.payment = payment;
+    this.createdAt = createdAt;
+    this.failureMessages = failureMessages;
+}
+
+public Payment getPayment() {
+    return payment;
+}
+
+public ZonedDateTime getCreatedAt() {
+    return createdAt;
+}
+
+public List<String> getFailureMessages() {
+    return failureMessages;
+}
+
+
+}
