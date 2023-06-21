474f4ace921cf3f5bdd7178bfd7412ddb09ce1c6
gra-m <grahamd483@gmail.com>
2023-06-21 15:16:45 +0200

    52. payment-messaging [M] listener.kafka.PaymentRequestKafkaListener

--- payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/ports/input/message/listener/PaymentRequestMessageListener.java
+++ payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment/service/domain/ports/input/message/listener/PaymentRequestMessageListener.java
@@ -5,11 +5,13 @@ import com.food.ordering.system.payment.service.domain.dto.PaymentRequest;
  * Ports?: These are nothing but interfaces that need to be implemented with adapters; there are two types of ports in
  * hexagonal architecture input and output.
  *
- * Input ports == Implemented here in the payment-application-service
+ * Input ports == Implemented in PaymentRequestMessageListenerImpl the payment-application-service
  * whereas output ports are implemented in infrastructure (currently kafka) via payment-messaging
  */
 public interface PaymentRequestMessageListener {
+
 void completePayment(PaymentRequest paymentRequest);
+
 void cancelPayment(PaymentRequest paymentRequest);
 
 
---
+++ payment-service/payment-messaging/src/main/java/com/food/ordering/system/payment/service/messaging/listener/kafka/PaymentRequestKafkaListener.java
@@ -0,0 +1,84 @@
+package com.food.ordering.system.payment.service.messaging.listener.kafka;
+
+import com.food.ordering.system.kafka.consumer.KafkaConsumer;
+import com.food.ordering.system.kafka.order.avro.model.PaymentOrderStatus;
+import com.food.ordering.system.kafka.order.avro.model.PaymentRequestAvroModel;
+import com.food.ordering.system.payment.service.domain.ports.input.message.listener.PaymentRequestMessageListener;
+import com.food.ordering.system.payment.service.messaging.mapper.PaymentMessagingDataMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * Listen for kafka messages and trigger using PaymentRequestMessageListenerImpl
+ */
+@Slf4j
+@Component
+public class PaymentRequestKafkaListener implements KafkaConsumer<PaymentRequestAvroModel> {
+
+    private final PaymentRequestMessageListener paymentRequestMessageListener;
+    private final PaymentMessagingDataMapper paymentMessagingDataMapper;
+
+public PaymentRequestKafkaListener(PaymentRequestMessageListener paymentRequestMessageListener,
+                                   PaymentMessagingDataMapper paymentMessagingDataMapper) {
+    this.paymentRequestMessageListener = paymentRequestMessageListener;
+    this.paymentMessagingDataMapper = paymentMessagingDataMapper;
+}
+
+
+/**
+ * @KafkaListener as per configured in payment-container module application.yml
+ *
+ * PENDING or CANCELLED if failed in approval phase (payment or restaurant order-fulfillment issue)
+ *
+ * PaymentRequestMessageListenerImpl in the payment-application-service fires the required events <i>after</i>
+ * the data has been persisted in the relevant local database.
+ *  payment-service/payment-domain/payment-application-service/src/main/java/com/food/ordering/system/payment
+ *  /service/domain/ports/input/message/listener/PaymentRequestMessageListenerImpl.java
+ *
+ * @param messages   A list of given Type of messages == Spring.messaging.handler.annotation @Payload
+ * @param keys       A list of Longs that are keys == Spring @Header
+ * @param partitions A list of Integers that are the partitions == Spring @Header
+ * @param offsets    A list of Longs that are the offsets == Spring @Header with KafkaHeaders SpringKafkaHelper
+ */
+@Override
+@KafkaListener(id = "${kafka-consumer-config.payment-consumer-group-id}",
+topics = "${payment-service.payment-request-topic-name}")
+public void receive(@Payload List<PaymentRequestAvroModel> messages,
+                    @Header(KafkaHeaders.RECEIVED_MESSAGE_KEY)List<String> keys,
+                    @Header(KafkaHeaders.RECEIVED_PARTITION_ID)List<Integer> partitions,
+                    @Header(KafkaHeaders.OFFSET)List<Long> offsets) {
+
+    log.info("{} number of payment requests received with keys: {}, partitions: {} and, offsets: {}",
+    messages.size(),
+    keys.toString(),
+    partitions.toString(),
+    offsets.toString());
+
+    // handle payload: fixme handled with string equals in a.n.other kafka listener, why ok here, but not there?
+    messages.forEach(paymentRequestAvroModel -> {
+        if (paymentRequestAvroModel.getPaymentOrderStatus() == PaymentOrderStatus.PENDING) {
+           log.info("Processing payment for order id: {}", paymentRequestAvroModel.getOrderId());
+
+           paymentRequestMessageListener.completePayment(paymentMessagingDataMapper
+           .paymentRequestAvroModelToPaymentRequest(paymentRequestAvroModel));
+
+        } else if( paymentRequestAvroModel.getPaymentOrderStatus() == PaymentOrderStatus.CANCELLED ) {
+            log.info("Cancelling payment for order id: {}", paymentRequestAvroModel.getOrderId());
+
+            paymentRequestMessageListener.cancelPayment(paymentMessagingDataMapper
+            .paymentRequestAvroModelToPaymentRequest(paymentRequestAvroModel));
+
+        }
+
+    });
+
+}
+
+
+}
