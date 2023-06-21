7c720c89548a03d9e8e9e96a89116a57410b2a6b
gra-m <grahamd483@gmail.com>
2023-06-21 18:20:37 +0200

    53b. [M] payment-service/payment-container, payment-domain-service made available to Spring, Application class added.

---
+++ payment-service/payment-container/src/main/java/com/food/ordering/system/payment/service/domain/BeanConfiguration.java
@@ -0,0 +1,18 @@
+package com.food.ordering.system.payment.service.domain;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * This is required because of the purposeful independence of the payment-domain-service module.
+ * Because it does not depend on Spring it has to be made available as a Spring Bean through @Configuration
+ */
+@Configuration
+public class BeanConfiguration {
+
+@Bean
+public PaymentDomainService paymentDomainService() {
+    return new PaymentDomainServiceImpl();
+}
+
+}
