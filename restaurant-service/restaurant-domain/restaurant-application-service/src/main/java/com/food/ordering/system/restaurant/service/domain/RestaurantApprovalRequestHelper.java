11d0da106d6c00247be8231cb5fdd5e60e2b0f8f
gra-m <grahamd483@gmail.com>
2023-06-22 16:38:14 +0200

    57a. [M]restaurant-application-service input ports RestaurantApprovalRequestHelper, Mapper and Impl

---
+++ restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/com/food/ordering/system/restaurant/service/domain/RestaurantApprovalRequestHelper.java
@@ -0,0 +1,93 @@
+package com.food.ordering.system.restaurant.service.domain;
+
+import com.food.ordering.system.domain.valueobject.OrderId;
+import com.food.ordering.system.restaurant.service.domain.dto.RestaurantApprovalRequest;
+import com.food.ordering.system.restaurant.service.domain.entity.Restaurant;
+import com.food.ordering.system.restaurant.service.domain.event.OrderApprovalEvent;
+import com.food.ordering.system.restaurant.service.domain.exception.RestaurantNotFoundException;
+import com.food.ordering.system.restaurant.service.domain.mapper.RestaurantDataMapper;
+import com.food.ordering.system.restaurant.service.domain.ports.output.message.publiser.OrderApprovedMessagePublisher;
+import com.food.ordering.system.restaurant.service.domain.ports.output.message.publiser.OrderRejectedMessagePublisher;
+import com.food.ordering.system.restaurant.service.domain.ports.output.repository.OrderApprovalRepository;
+import com.food.ordering.system.restaurant.service.domain.ports.output.repository.RestaurantRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@Component
+public class RestaurantApprovalRequestHelper {
+private final RestaurantDomainService restaurantDomainService;
+private final RestaurantDataMapper restaurantDataMapper;
+private final RestaurantRepository restaurantRepository;
+private final OrderApprovalRepository orderApprovalRepository;
+private final OrderApprovedMessagePublisher orderApprovedMessagePublisher;
+private final OrderRejectedMessagePublisher orderRejectedMessagePublisher;
+
+
+public RestaurantApprovalRequestHelper(RestaurantDomainService restaurantDomainService,
+                                       RestaurantDataMapper restaurantDataMapper,
+                                       RestaurantRepository restaurantRepository,
+                                       OrderApprovalRepository orderApprovalRepository,
+                                       OrderApprovedMessagePublisher orderApprovedMessagePublisher,
+                                       OrderRejectedMessagePublisher orderRejectedMessagePublisher) {
+    this.restaurantDomainService = restaurantDomainService;
+    this.restaurantDataMapper = restaurantDataMapper;
+    this.restaurantRepository = restaurantRepository;
+    this.orderApprovalRepository = orderApprovalRepository;
+    this.orderApprovedMessagePublisher = orderApprovedMessagePublisher;
+    this.orderRejectedMessagePublisher = orderRejectedMessagePublisher;
+}
+
+@Transactional
+public OrderApprovalEvent persistOrderApproval(RestaurantApprovalRequest restaurantApprovalRequest) {
+    log.info("Processing restaurant approval for order id: {}", restaurantApprovalRequest.getOrderId());
+    List<String> failureMessages = Collections.emptyList();
+    Restaurant restaurant = findRestaurant(restaurantApprovalRequest);
+
+    OrderApprovalEvent orderApprovalEvent = restaurantDomainService.validateOrder(restaurant, failureMessages,
+    orderApprovedMessagePublisher,orderRejectedMessagePublisher);
+
+    orderApprovalRepository.save(restaurant.getOrderApproval());
+
+    return orderApprovalEvent;
+}
+
+private Restaurant findRestaurant(RestaurantApprovalRequest restaurantApprovalRequest) {
+    Restaurant restaurant =
+    restaurantDataMapper.restaurantApprovalRequestToRestaurant(restaurantApprovalRequest);
+
+    Optional<Restaurant> restaurantResult = restaurantRepository.findRestaurantInformation(restaurant);
+
+    if(restaurantResult.isEmpty()) {
+        log.error("RestaurantApprovalRequestHelper says: Restaurant with id {} not found!",
+        restaurant.getId().getValue());
+
+        throw new RestaurantNotFoundException(String.format("Restaurant with id %s not found!",
+        restaurant.getId().getValue()));
+    }
+
+    //update the restaurant with up-to-date info from the restaurant database.
+    Restaurant restaurantEntity = restaurantResult.get();
+    restaurant.setActive(restaurantEntity.isActive());
+    restaurant.getOrderDetail().getProducts().forEach(product -> {
+        restaurantEntity.getOrderDetail().getProducts().forEach(p -> {
+            if(p.getId().equals(product.getId())) {
+                product.updateWithConfirmedNamePriceAndAvailability(p.getName(), p.getPrice(), p.isAvailable());
+            }
+        });
+    });
+
+    restaurant.getOrderDetail().setId(new OrderId(UUID.fromString(restaurantApprovalRequest.getOrderId())));
+
+    return restaurant;
+
+}
+
+
+}
