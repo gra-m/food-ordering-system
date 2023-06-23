8300b39b4ea3d395e5318986938febbf38259d6d
gra-m <grahamd483@gmail.com>
2023-06-23 10:56:20 +0200

    59. [M] restaurant-service/restaurant-messaging

--- restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/com/food/ordering/system/restaurant/service/domain/ports/output/message/publiser/OrderApprovedMessagePublisher.java
+++ restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/com/food/ordering/system/restaurant/service/domain/ports/output/message/publiser/OrderApprovedMessagePublisher.java
@@ -3,6 +3,11 @@ package com.food.ordering.system.restaurant.service.domain.ports.output.message.
 import com.food.ordering.system.domain.event.publisher.DomainEventPublisher;
 import com.food.ordering.system.restaurant.service.domain.event.OrderApprovedEvent;
 
+/**
+ * An output port that is implemented in restaurant-service/restaurant-messaging publisher.kafka
+ * .OrderApprovedKafkaMessagePublisher.
+ * So, implementation to another messaging service possible there.
+ */
 public interface OrderApprovedMessagePublisher extends DomainEventPublisher<OrderApprovedEvent> {
 
 
