11e7a401b44b86a5efb3dca2f52b0d9e54524174
gra-m <grahamd483@gmail.com>
2023-06-26 15:03:53 +0200

    63a. SAGA [M] order-service->order-application-service.domain.ports.input.message.listener.PaymentResponseMessageListenerImpl

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/input/message/listener/payment/PaymentResponseMessageListenerImpl.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/input/message/listener/payment/PaymentResponseMessageListenerImpl.java
@@ -1,28 +1,33 @@
 package com.food.ordering.system.order.service.domain.ports.input.message.listener.payment;
 
+import com.food.ordering.system.order.service.domain.OrderPaymentSaga;
 import com.food.ordering.system.order.service.domain.dto.message.PaymentResponse;
+import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
-/**
- * Adapter.
- *
- * fixme - early note
- * Triggered by other domain events from other bound contexts BeanA [here] to BeanB[@Transactional] in
- * separate helper. SAGA complete once implemented
- */
+import static com.food.ordering.system.order.service.domain.entity.Order.FAILURE_MESSAGE_DELIMITER;
+
 
 @Slf4j
 @Validated
 @Service
 public class PaymentResponseMessageListenerImpl implements PaymentResponseMessageListener {
+private final OrderPaymentSaga orderPaymentSaga;
+
+public PaymentResponseMessageListenerImpl(OrderPaymentSaga orderPaymentSaga) {
+    this.orderPaymentSaga = orderPaymentSaga;
+}
 
 /**
  * @param paymentResponse
  */
 @Override
 public void paymentCompleted(PaymentResponse paymentResponse) {
+    OrderPaidEvent domainEvent = orderPaymentSaga.process(paymentResponse);
+    log.info("Publishing OrderPaidEvent for order id: {}", paymentResponse.getOrderId());
+    domainEvent.fire();
 
 }
 
@@ -31,6 +36,10 @@ public void paymentCompleted(PaymentResponse paymentResponse) {
  */
 @Override
 public void paymentCancelled(PaymentResponse paymentResponse) {
-
+    orderPaymentSaga.rollback(paymentResponse);
+    log.info("Order is roll backed with failure messages: {}",
+    String.join(FAILURE_MESSAGE_DELIMITER, paymentResponse.getFailureMessages()));
 }
+
+
 }
