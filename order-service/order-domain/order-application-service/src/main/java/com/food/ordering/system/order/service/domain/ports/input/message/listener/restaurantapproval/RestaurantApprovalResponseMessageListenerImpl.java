f0d2b1943fd0c7c372c578804a0c211d3041f12f
gra-m <grahamd483@gmail.com>
2023-06-26 17:56:37 +0200

    63c. SAGA [M] order-service->order-application RestaurantApprovalResponseMessageListenerImpl. Saga steps complete OrderPayment/Approval

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/input/message/listener/restaurantapproval/RestaurantApprovalResponseMessageListenerImpl.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/input/message/listener/restaurantapproval/RestaurantApprovalResponseMessageListenerImpl.java
@@ -1,11 +1,17 @@
 package com.food.ordering.system.order.service.domain.ports.input.message.listener.restaurantapproval;
 
+import com.food.ordering.system.order.service.domain.OrderApprovalSaga;
 import com.food.ordering.system.order.service.domain.dto.message.RestaurantApprovalResponse;
+import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
+import static com.food.ordering.system.order.service.domain.entity.Order.FAILURE_MESSAGE_DELIMITER;
+
 /**
+ * The order messaging module uses this interface after consuming restaurantApprovalResponseKafkaTopic.
+ *
  * Triggered by other domain events from other bound contexts BeanA [here] to BeanB[@Transactional] in
  * separate helper. SAGA complete once implemented
  */
@@ -14,13 +20,20 @@ import org.springframework.validation.annotation.Validated;
 @Validated
 @Service
 public class RestaurantApprovalResponseMessageListenerImpl implements RestaurantApprovalResponseMessageListener{
+private final OrderApprovalSaga orderApprovalSaga;
+
+public RestaurantApprovalResponseMessageListenerImpl(OrderApprovalSaga orderApprovalSaga) {
+    this.orderApprovalSaga = orderApprovalSaga;
+}
+
 
 /**
  * @param restaurantApprovalResponse
  */
 @Override
 public void orderApproved(RestaurantApprovalResponse restaurantApprovalResponse) {
-
+    orderApprovalSaga.process(restaurantApprovalResponse);
+    log.info("Order is approved for order id {}", restaurantApprovalResponse.getOrderId());
 }
 
 /**
@@ -28,6 +41,12 @@ public void orderApproved(RestaurantApprovalResponse restaurantApprovalResponse)
  */
 @Override
 public void orderRejected(RestaurantApprovalResponse restaurantApprovalResponse) {
+    OrderCancelledEvent domainEvent = orderApprovalSaga.rollback(restaurantApprovalResponse);
+    log.info("Publishing order cancelled event for order id: {} with failure messages: {}",
+    restaurantApprovalResponse.getOrderId(),
+    String.join(FAILURE_MESSAGE_DELIMITER, restaurantApprovalResponse.getFailureMessages()));
+
+    domainEvent.fire();
 
 }
 }
