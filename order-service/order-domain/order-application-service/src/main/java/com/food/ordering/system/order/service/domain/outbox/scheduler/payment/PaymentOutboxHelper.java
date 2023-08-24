b95d6850f18f0679d2711a4667a76973e33e113e
gra-m <grahamd483@gmail.com>
2023-08-24 22:01:41 +0200

    69c. PaymentOutboxHelper

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/outbox/scheduler/payment/PaymentOutboxHelper.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/outbox/scheduler/payment/PaymentOutboxHelper.java
@@ -1,14 +1,19 @@
 package com.food.ordering.system.order.service.domain.outbox.scheduler.payment;
 
 import com.food.ordering.system.order.service.domain.outbox.model.payment.OrderPaymentOutboxMessage;
+import com.food.ordering.system.order.service.domain.ports.output.repository.OrderRepository;
 import com.food.ordering.system.order.service.domain.ports.output.repository.PaymentOutboxRepository;
 import com.food.ordering.system.outbox.OutboxStatus;
 import com.food.ordering.system.saga.SagaStatus;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
+
+import static com.food.ordering.system.saga.order.SagaConstants.ORDER_SAGA_NAME;
 @Slf4j
 @Component
 public class PaymentOutboxHelper {
@@ -18,9 +23,17 @@ public PaymentOutboxHelper(PaymentOutboxRepository paymentOutboxRepository) {
     this.paymentOutboxRepository = paymentOutboxRepository;
 }
 
+@Transactional(readOnly = true)
 public Optional<List<OrderPaymentOutboxMessage>> getPaymentOutboxMessageByOutboxStatusAndSagaStatus(
 OutboxStatus outboxStatus, SagaStatus... sagaStatus) {
-    return null;
+    return paymentOutboxRepository.findByTypeAndOutboxStatusAndSagaStatus(ORDER_SAGA_NAME, outboxStatus, sagaStatus);
+}
+
+@Transactional(readOnly = true)
+public Optional<OrderPaymentOutboxMessage> getPaymentOutboxMessageBySagaIdAndSagaStatus(UUID sagaId,
+                                                                                        SagaStatus... sagaStatus) {
+    return paymentOutboxRepository.findByTypeAndSagaIdAndSagaStatus(ORDER_SAGA_NAME, sagaId, sagaStatus);
+
 }
 
 
