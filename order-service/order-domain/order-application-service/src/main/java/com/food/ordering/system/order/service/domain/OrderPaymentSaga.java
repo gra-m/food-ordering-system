07f7ee40d8a82011ec75fc42a5fb67b19397282c
gra-m <grahamd483@gmail.com>
2023-08-29 12:37:03 +0200

    73c. Event no longer fired in paymentCompleted method, with OUTBOX the event is instead saved to local outbox table and then -> this is read and published by the scheduler. The weakness of SAGA pattern and is alleviated with the messaging and order database save taking place in the same ACID transaction.

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderPaymentSaga.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/OrderPaymentSaga.java
@@ -4,16 +4,17 @@ import com.food.ordering.system.domain.valueobject.OrderStatus;
 import com.food.ordering.system.order.service.domain.dto.message.PaymentResponse;
 import com.food.ordering.system.order.service.domain.entity.Order;
 import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
-import com.food.ordering.system.order.service.domain.exception.OrderNotFoundException;
+import com.food.ordering.system.order.service.domain.mapper.OrderDataMapper;
 import com.food.ordering.system.order.service.domain.outbox.model.payment.OrderPaymentOutboxMessage;
+import com.food.ordering.system.order.service.domain.outbox.scheduler.approval.ApprovalOutboxHelper;
 import com.food.ordering.system.order.service.domain.outbox.scheduler.payment.PaymentOutboxHelper;
+import com.food.ordering.system.outbox.OutboxStatus;
 import com.food.ordering.system.saga.SagaStatus;
 import com.food.ordering.system.saga.SagaStep;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.management.openmbean.OpenDataException;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Optional;
@@ -32,34 +33,39 @@ import static com.food.ordering.system.domain.DomainConstants.UTCBRU;
 @Slf4j
 @Component
 public class OrderPaymentSaga implements SagaStep<PaymentResponse> {
-private final OrderDomainService orderDomainService;
-private final OrderSagaHelper orderSagaHelper;
-private final PaymentOutboxHelper paymentOutboxHelper;
+    private final OrderDomainService orderDomainService;
+    private final OrderSagaHelper orderSagaHelper;
+    private final PaymentOutboxHelper paymentOutboxHelper;
+    private final ApprovalOutboxHelper approvalOutboxHelper;
+    private final OrderDataMapper orderDataMapper;
 
-public OrderPaymentSaga(OrderDomainService orderDomainService,
-                        OrderSagaHelper orderSagaHelper,
-                        PaymentOutboxHelper paymentOutboxHelper) {
-    this.orderDomainService = orderDomainService;
-    this.orderSagaHelper = orderSagaHelper;
-    this.paymentOutboxHelper = paymentOutboxHelper;
-}
+    public OrderPaymentSaga(OrderDomainService orderDomainService,
+                            OrderSagaHelper orderSagaHelper,
+                            PaymentOutboxHelper paymentOutboxHelper,
+                            ApprovalOutboxHelper approvalOutboxHelper, OrderDataMapper orderDataMapper) {
+        this.orderDomainService = orderDomainService;
+        this.orderSagaHelper = orderSagaHelper;
+        this.paymentOutboxHelper = paymentOutboxHelper;
+        this.approvalOutboxHelper = approvalOutboxHelper;
+        this.orderDataMapper = orderDataMapper;
+    }
 
-/**
- * Called from ListenerImpl classes for payment and restaurant approval, so it is called after a response is received from
- * the kafka payment response topic used in PaymentResponseKafkaListener and...
- * THIS MEANS an outbox message is already available, to read it from the database PaymentOutboxHelper is used.
- *
- * Restaurant Approval equivalent = restaurant approval topics/RestaurantApprovalRequestKafkaListener
- *
- * @param paymentResponse PaymentResponse data to be processed
- * @return OrderPaidEvent -> The DomainEvent returned after processing
- */
-@Override
-@Transactional
-public void process(PaymentResponse paymentResponse) {
-    // Retrieve pending == started order
-    Optional<OrderPaymentOutboxMessage> orderPaymentOutboxMessageResponse =
-    paymentOutboxHelper.getPaymentOutboxMessageBySagaIdAndSagaStatus(UUID.fromString(paymentResponse.getSagaId()), SagaStatus.STARTED);
+    /**
+     * Called from ListenerImpl classes for payment and restaurant approval, so it is called after a response is received from
+     * the kafka payment response topic used in PaymentResponseKafkaListener and...
+     * THIS MEANS an outbox message is already available, to read it from the database PaymentOutboxHelper is used.
+     * <p>
+     * Restaurant Approval equivalent = restaurant approval topics/RestaurantApprovalRequestKafkaListener
+     *
+     * @param paymentResponse PaymentResponse data to be processed
+     * @return OrderPaidEvent -> The DomainEvent returned after processing
+     */
+    @Override
+    @Transactional
+    public void process(PaymentResponse paymentResponse) {
+        // Retrieve pending == started order
+        Optional<OrderPaymentOutboxMessage> orderPaymentOutboxMessageResponse =
+                paymentOutboxHelper.getPaymentOutboxMessageBySagaIdAndSagaStatus(UUID.fromString(paymentResponse.getSagaId()), SagaStatus.STARTED);
 
     /* if empty it has already been processed and this is an erroneous subsequent call to process()
     1. could happen when order-service sends the same outbox message to the payment service,
@@ -67,61 +73,69 @@ public void process(PaymentResponse paymentResponse) {
     3. which could happen when you have multiple instances of the order service.
     SO checking SagaStatus.Started catches this edge-case
     */
-    if (orderPaymentOutboxMessageResponse.isEmpty()) {
-        log.info("An outbox message with saga id: {} is already processed!", paymentResponse.getSagaId());
-        return;
+        if (orderPaymentOutboxMessageResponse.isEmpty()) {
+            log.info("An outbox message with saga id: {} is already processed!", paymentResponse.getSagaId());
+            return;
+        }
+
+        OrderPaymentOutboxMessage orderPaymentOutboxMessage = orderPaymentOutboxMessageResponse.get();
+
+        log.info("Completing payment for order with id: {}", paymentResponse.getOrderId());
+
+        Order order = orderSagaHelper.findOrder(paymentResponse.getOrderId());
+        OrderPaidEvent domainEvent = orderDomainService.payOrder(order);
+        orderSagaHelper.saveOrder(order);
+
+        SagaStatus sagaStatus = orderSagaHelper.orderStatusToSagaStatus(domainEvent.getOrder().getOrderStatus());
+
+        paymentOutboxHelper.save(getUpdatedPaymentOutboxMessage(orderPaymentOutboxMessage, domainEvent.getOrder().getOrderStatus(), sagaStatus));
+
+        // Now with outbox, event is no longer fired, local OUTBOX db is updated.
+
+        approvalOutboxHelper.saveApprovalOutboxMessage(orderDataMapper.OrderPaidEventToOrderApprovalEventPayload(domainEvent), domainEvent.getOrder().getOrderStatus(),
+                sagaStatus,
+                OutboxStatus.STARTED,
+                UUID.randomUUID());
+
+        log.info("Order with id {} is paid", order.getId().getValue());
     }
-
-    OrderPaymentOutboxMessage orderPaymentOutboxMessage = orderPaymentOutboxMessageResponse.get();
-
-    log.info("Completing payment for order with id: {}", paymentResponse.getOrderId());
-
-    Order order = orderSagaHelper.findOrder(paymentResponse.getOrderId());
-    OrderPaidEvent domainEvent = orderDomainService.payOrder(order);
-    orderSagaHelper.saveOrder(order);
-
-    SagaStatus sagaStatus = orderSagaHelper.orderStatusToSagaStatus(domainEvent.getOrder().getOrderStatus());
-
-    paymentOutboxHelper.save(getUpdatedPaymentOutboxMessage(orderPaymentOutboxMessage, domainEvent.getOrder().getOrderStatus(), sagaStatus));
-
-    log.info("Order with id {} is paid", order.getId().getValue());
-}
 
     /**
      * To update local retrieve orderPayementOutboxMessage with new status and time-stamp of this action.
+     *
      * @param orderPaymentOutboxMessage
-     * @param orderStatus the orderStatus retrieved from Domain event
-     * @param sagaStatus  new orderStatus retrieved from DomainEvent translated with orderSagaHelper
+     * @param orderStatus               the orderStatus retrieved from Domain event
+     * @param sagaStatus                new orderStatus retrieved from DomainEvent translated with orderSagaHelper
      * @return
      */
     private OrderPaymentOutboxMessage getUpdatedPaymentOutboxMessage(OrderPaymentOutboxMessage orderPaymentOutboxMessage, OrderStatus orderStatus, SagaStatus sagaStatus) {
 
-    orderPaymentOutboxMessage.setProcessedAt(ZonedDateTime.now(ZoneId.of(UTCBRU)));
-    orderPaymentOutboxMessage.setOrderStatus(orderStatus);
-    orderPaymentOutboxMessage.setSagaStatus(sagaStatus);
+        orderPaymentOutboxMessage.setProcessedAt(ZonedDateTime.now(ZoneId.of(UTCBRU)));
+        orderPaymentOutboxMessage.setOrderStatus(orderStatus);
+        orderPaymentOutboxMessage.setSagaStatus(sagaStatus);
 
-    return orderPaymentOutboxMessage;
+        return orderPaymentOutboxMessage;
     }
 
 
     /**
- * Rollback the action for this SAGA step 1
- *
- * @param paymentResponse PaymentResponseData to be 'rolled back' empty here as this is first step
- * @return EmptyEvent as this is the last rollback in the transaction
- */
-@Override
-@Transactional
-public void rollback(PaymentResponse paymentResponse) {
-    String orderId = paymentResponse.getOrderId();
-    log.info("[SAGA1 rollback payment-response -to-> EmptyEvent pre-save] Cancelling order with id: {}", orderId);
-    Order order = orderSagaHelper.findOrder(orderId);
-    orderDomainService.cancelOrder(order, paymentResponse.getFailureMessages());
-    orderSagaHelper.saveOrder(order);
+     * Rollback the action for this SAGA step 1
+     *
+     * @param paymentResponse PaymentResponseData to be 'rolled back' empty here as this is first step
+     * @return EmptyEvent as this is the last rollback in the transaction
+     */
+    @Override
+    @Transactional
+    public void rollback(PaymentResponse paymentResponse) {
+        String orderId = paymentResponse.getOrderId();
+        log.info("[SAGA1 rollback payment-response -to-> EmptyEvent pre-save] Cancelling order with id: {}", orderId);
+        Order order = orderSagaHelper.findOrder(orderId);
+        orderDomainService.cancelOrder(order, paymentResponse.getFailureMessages());
+        orderSagaHelper.saveOrder(order);
 
-    log.info("[SAGA1 rollback payment-response -to-> EmptyEvent post-save] order with id: {} cancelled", orderId);
+        log.info("[SAGA1 rollback payment-response -to-> EmptyEvent post-save] order with id: {} cancelled", orderId);
 
-}
+    }
 
 
 }
