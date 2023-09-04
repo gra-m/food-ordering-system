4740b954a622ad5c0d02810522dcec5041ba9a90
gra-m <grahamd483@gmail.com>
2023-09-04 19:43:08 +0200

    78b No copy paste here

---
+++ order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/outbox/restaurantapproval/exception/ApprovalOutboxNotFoundException.java
@@ -0,0 +1,12 @@
+package com.food.ordering.system.order.service.dataaccess.outbox.restaurantapproval.exception;
+
+public class ApprovalOutboxNotFoundException extends RuntimeException{
+
+    public ApprovalOutboxNotFoundException(String message, Exception e) {
+        super(message, e);
+    }
+
+    public ApprovalOutboxNotFoundException(String message) {
+        super(message);
+    }
+}
