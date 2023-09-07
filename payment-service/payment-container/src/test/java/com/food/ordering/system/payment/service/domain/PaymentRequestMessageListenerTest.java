d2b94152b5874be47a0c1cb07b9c8dd92dab9fd3
gra-m <grahamd483@gmail.com>
2023-09-07 15:46:08 +0200

    87c. now executing threads x2 reliably creating sql state 23505 unique constraint violation

--- payment-service/payment-container/src/test/java/com/food/ordering/system/payment/service/domain/PaymentRequestMessageListenerTest.java
+++ payment-service/payment-container/src/test/java/com/food/ordering/system/payment/service/domain/PaymentRequestMessageListenerTest.java
@@ -15,9 +15,10 @@ import org.springframework.dao.DataAccessException;
 
 import java.math.BigDecimal;
 import java.time.Instant;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static com.food.ordering.system.saga.order.SagaConstants.ORDER_SAGA_NAME;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -53,9 +54,49 @@ public class PaymentRequestMessageListenerTest
         }
 
         assertOrderOutbox(sagaId);
-
-
     }
+
+    @Test
+    void testDoublePaymentWithThreads()
+    {
+        String sagaId = UUID.randomUUID().toString();
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        List<Callable<Object>> tasks = new ArrayList<>();
+
+        try {
+            tasks.add(Executors.callable(() ->
+                    {
+                        try {
+                            paymentRequestMessageListener.completePayment(getPaymentRequest(sagaId));
+                        } catch (DataAccessException e) {
+                            log.error("DataAccessException occurred for thread 1 with sql state: {}",
+                                    ((PSQLException) Objects.requireNonNull(e.getRootCause())).getSQLState());
+                        }
+                    }
+                    ));
+
+            tasks.add(Executors.callable(() ->
+                    {
+                        try {
+                            paymentRequestMessageListener.completePayment(getPaymentRequest(sagaId));
+                        } catch (DataAccessException e) {
+                            log.error("DataAccessException occurred for thread 2 with sql state: {}",
+                                    ((PSQLException) Objects.requireNonNull(e.getRootCause())).getSQLState());
+                        }
+                    }
+            ));
+
+            // invokes the callables, pausing main thread until they are resolved.
+            executor.invokeAll(tasks);
+
+            assertOrderOutbox(sagaId);
+        } catch (InterruptedException e) {
+            log.error("Error calling complete payment!", e);
+        } finally {
+                executor.shutdown();
+        }
+    }
+
 
     private void assertOrderOutbox(String sagaId)
     {
